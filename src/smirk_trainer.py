2bf039ff81da82ee973c3aa03c52217abc271400
LCX@ARGOS <911239555@qq.com>
2025-12-01 19:59:32 +0800

    Update smirk_trainer.py
    LCX251201替换掉STEP2方法。

--- src/smirk_trainer.py
+++ src/smirk_trainer.py
@@ -190,7 +190,6 @@ class SmirkTrainer(BaseTrainer):
         Ke = self.config.train.Ke
         
         # start from the same encoder output and add noise to expression params
-        # hard clone flame_feats
         flame_feats = {}
         for k, v in encoder_output.items():
             tmp = v.clone().detach()
@@ -198,138 +197,78 @@ class SmirkTrainer(BaseTrainer):
 
         # split Ke * B into 4 random groups        
         gids = torch.randperm(Ke * B)
-        # 4 groups
-        gids = [gids[:Ke * B // 4], gids[Ke * B // 4: 2 * Ke * B // 4], gids[2 * Ke * B // 4: 3 * Ke * B // 4], gids[3 * Ke * B // 4:]] 
+        gids = [gids[:Ke * B // 4], gids[Ke * B // 4: 2 * Ke * B // 4], 
+                gids[2 * Ke * B // 4: 3 * Ke * B // 4], gids[3 * Ke * B // 4:]] 
 
         feats_dim = flame_feats['expression_params'].size(1)        
 
         # ---------------- random expression ---------------- #
-        # 1 of 4 Ke - random expressions!  
         param_mask = torch.bernoulli(torch.ones((len(gids[0]), feats_dim)) * 0.5).to(self.config.device)
-        
-        new_expressions = (torch.randn((len(gids[0]), feats_dim)).to(self.config.device)) * (1 + 2 * torch.rand((len(gids[0]), 1)).to(self.config.device)) * param_mask + flame_feats['expression_params'][gids[0]]
-        flame_feats['expression_params'][gids[0]] = torch.clamp(new_expressions, -4.0, 4.0) +  (0 + 0.2 * torch.rand((len(gids[0]), 1)).to(self.config.device)) * torch.randn((len(gids[0]), feats_dim)).to(self.config.device)
+        new_expressions = (torch.randn((len(gids[0]), feats_dim)).to(self.config.device)) * \
+                          (1 + 2 * torch.rand((len(gids[0]), 1)).to(self.config.device)) * param_mask + \
+                          flame_feats['expression_params'][gids[0]]
+        flame_feats['expression_params'][gids[0]] = torch.clamp(new_expressions, -4.0, 4.0) + \
+                                                    (0 + 0.2 * torch.rand((len(gids[0]), 1)).to(self.config.device)) * \
+                                                    torch.randn((len(gids[0]), feats_dim)).to(self.config.device)
         
         # ---------------- permutation of expression ---------------- #
-        # 2 of 4 Ke - permutation!    + extra noise 
-        flame_feats['expression_params'][gids[1]] = (0.25 + 1.25 * torch.rand((len(gids[1]), 1)).to(self.config.device)) * flame_feats['expression_params'][gids[1]][torch.randperm(len(gids[1]))] + \
-                                        (0 + 0.2 * torch.rand((len(gids[1]), 1)).to(self.config.device)) *  torch.randn((len(gids[1]), feats_dim)).to(self.config.device)
+        flame_feats['expression_params'][gids[1]] = (0.25 + 1.25 * torch.rand((len(gids[1]), 1)).to(self.config.device)) * \
+                                                    flame_feats['expression_params'][gids[1]][torch.randperm(len(gids[1]))] + \
+                                                    (0 + 0.2 * torch.rand((len(gids[1]), 1)).to(self.config.device)) * \
+                                                    torch.randn((len(gids[1]), feats_dim)).to(self.config.device)
         
         # ---------------- template injection ---------------- #
-        # 3 of 4 Ke - template injection!  + extra noise
         for i in range(len(gids[2])):
             expression = self.load_random_template(num_expressions=self.config.arch.num_expression)
-            flame_feats['expression_params'][gids[2][i],:self.config.arch.num_expression] = (0.25 + 1.25 * torch.rand((1, 1)).to(self.config.device)) * torch.Tensor(expression).to(self.config.device)
-        flame_feats['expression_params'][gids[2]] += (0 + 0.2 * torch.rand((len(gids[2]), 1)).to(self.config.device)) * torch.randn((len(gids[2]), feats_dim)).to(self.config.device)
+            flame_feats['expression_params'][gids[2][i],:self.config.arch.num_expression] = \
+                (0.25 + 1.25 * torch.rand((1, 1)).to(self.config.device)) * torch.Tensor(expression).to(self.config.device)
+        flame_feats['expression_params'][gids[2]] += (0 + 0.2 * torch.rand((len(gids[2]), 1)).to(self.config.device)) * \
+                                                     torch.randn((len(gids[2]), feats_dim)).to(self.config.device)
 
-        # ---------------- tweak jaw for all paths ---------------- #
-        scale_mask = torch.Tensor([1, .1, .1]).to(self.config.device).view(1, 3) * torch.bernoulli(torch.ones(Ke * B) * 0.5).to(self.config.device).view(-1, 1)
-        flame_feats['jaw_params'] = flame_feats['jaw_params']  + torch.randn(flame_feats['jaw_params'].size()).to(self.config.device) * 0.2 * scale_mask
-        flame_feats['jaw_params'][..., 0] = torch.clamp(flame_feats['jaw_params'][..., 0] , 0.0, 0.5)
+        # ---------------- tweak jaw ---------------- #
+        scale_mask = torch.Tensor([1, .1, .1]).to(self.config.device).view(1, 3) * \
+                     torch.bernoulli(torch.ones(Ke * B) * 0.5).to(self.config.device).view(-1, 1)
+        flame_feats['jaw_params'] = flame_feats['jaw_params'] + \
+                                    torch.randn(flame_feats['jaw_params'].size()).to(self.config.device) * 0.2 * scale_mask
+        flame_feats['jaw_params'][..., 0] = torch.clamp(flame_feats['jaw_params'][..., 0], 0.0, 0.5)
         
-        # ---------------- tweak eyelids for all paths ---------------- #
+        # ---------------- tweak eyelids ---------------- #
         if self.config.arch.use_eyelids:
             flame_feats['eyelid_params'] += (-1 + 2 * torch.rand(size=flame_feats['eyelid_params'].size()).to(self.config.device)) * 0.25
             flame_feats['eyelid_params'] = torch.clamp(flame_feats['eyelid_params'], 0.0, 1.0)
 
         # ---------------- zero expression ---------------- #
-        # 4 of 4 Ke - zero expression!     
-        # let the eyelids to move a lot - extra noise
         flame_feats['expression_params'][gids[3]] *= 0.0
-        flame_feats['expression_params'][gids[3]] += (0 + 0.2 * torch.rand((len(gids[3]), 1)).to(self.config.device)) * torch.randn((len(gids[3]), flame_feats['expression_params'].size(1))).to(self.config.device)
-        
+        flame_feats['expression_params'][gids[3]] += (0 + 0.2 * torch.rand((len(gids[3]), 1)).to(self.config.device)) * \
+                                                     torch.randn((len(gids[3]), flame_feats['expression_params'].size(1))).to(self.config.device)
         flame_feats['jaw_params'][gids[3]] *= 0.0
         flame_feats['eyelid_params'][gids[3]] = torch.rand(size=flame_feats['eyelid_params'][gids[3]].size()).to(self.config.device)        
 
-        flame_feats['expression_params'] = flame_feats['expression_params'].detach()
-        flame_feats['pose_params'] = flame_feats['pose_params'].detach()
-        flame_feats['shape_params'] = flame_feats['shape_params'].detach()
-        flame_feats['jaw_params'] = flame_feats['jaw_params'].detach()
-        flame_feats['eyelid_params'] = flame_feats['eyelid_params'].detach()
+        # detach
+        for k in ['expression_params','pose_params','shape_params','jaw_params','eyelid_params']:
+            flame_feats[k] = flame_feats[k].detach()
 
-        # after defining param augmentation, we can render the new faces
+        # ---------------- render ---------------- #
         with torch.no_grad():
             flame_output = self.flame.forward(encoder_output)
             rendered_output = self.renderer.forward(flame_output['vertices'], encoder_output['cam'])
             flame_output.update(rendered_output)
-     
+
             # render the tweaked face
             flame_output_2nd_path = self.flame.forward(flame_feats)
-            renderer_output_2nd_path = self.renderer.forward(flame_output_2nd_path['vertices'], encoder_output['cam'])
+
+            # 相机参数扩展到 Ke * B
+            cam = encoder_output['cam']
+            Bv = flame_output_2nd_path['vertices'].shape[0]   # 128
+            Bc = cam.shape[0]                                 # 32
+            if Bv % Bc == 0:
+                repeat_times = Bv // Bc
+                cam_expanded = cam.repeat(repeat_times, 1)
+            else:
+                raise RuntimeError(f"Batch mismatch: verts B={Bv}, cam B={Bc}")
+
+            renderer_output_2nd_path = self.renderer.forward(flame_output_2nd_path['vertices'], cam_expanded)
             rendered_img_2nd_path = renderer_output_2nd_path['rendered_img'].detach()
-
-            
-            # sample points for the image reconstruction
-
-            # with transfer pixel we can use more points if needed in cycle to quickly learn realistic generations!
-            tmask_ratio = self.config.train.mask_ratio #* 2.0
-
-            # use the initial flame estimation to sample points from the initial image
-            points1, sampled_coords = masking_utils.mesh_based_mask_uniform_faces(flame_output['transformed_vertices'], 
-                                                                     flame_faces=self.flame.faces_tensor,
-                                                                     face_probabilities=self.face_probabilities,
-                                                                     mask_ratio=tmask_ratio)
-            
-           
-            # apply repeat on sampled_coords elements
-            sampled_coords['sampled_faces_indices'] = sampled_coords['sampled_faces_indices'].repeat(Ke, 1)
-            sampled_coords['barycentric_coords'] = sampled_coords['barycentric_coords'].repeat(Ke, 1, 1)
-            
-            # get the sampled points that correspond to the face deformations
-            points2, sampled_coords = masking_utils.mesh_based_mask_uniform_faces(renderer_output_2nd_path['transformed_vertices'], 
-                                                                     flame_faces=self.flame.faces_tensor,
-                                                                     face_probabilities=self.face_probabilities,
-                                                                     mask_ratio=tmask_ratio,
-                                                                     coords=sampled_coords)
-            
-
-            # transfer pixels from initial image to the new image
-            extra_points = masking_utils.transfer_pixels(img.repeat(Ke, 1, 1, 1), points1.repeat(Ke, 1, 1), points2)
-        
-            
-            rendered_mask = (rendered_img_2nd_path > 0).all(dim=1, keepdim=True).float()
-                
-        masked_img_2nd_path = masking_utils.masking(img.repeat(Ke, 1, 1, 1), masks.repeat(Ke, 1, 1, 1), extra_points, self.config.train.mask_dilation_radius, 
-                                      rendered_mask=rendered_mask, extra_noise=True, random_mask=0.005)
-        
-        reconstructed_img_2nd_path = self.smirk_generator(torch.cat([rendered_img_2nd_path, masked_img_2nd_path], dim=1).detach())
-        if self.config.train.freeze_generator_in_second_path:
-            reconstructed_img_2nd_path = reconstructed_img_2nd_path.detach()
-
-        
-        recon_feats = self.smirk_encoder(reconstructed_img_2nd_path.view(Ke * B, C, H, W)) 
-
-        flame_output_2nd_path_2 = self.flame.forward(recon_feats)
-        rendered_img_2nd_path_2 = self.renderer.forward(flame_output_2nd_path_2['vertices'], recon_feats['cam'])['rendered_img']
-
-        losses = {}
-        
-        cycle_loss = 1.0 * F.mse_loss(recon_feats['expression_params'], flame_feats['expression_params']) + \
-                     10.0 * F.mse_loss(recon_feats['jaw_params'], flame_feats['jaw_params'])
-        
-        if self.config.arch.use_eyelids:
-            cycle_loss += 10.0 * F.mse_loss(recon_feats['eyelid_params'], flame_feats['eyelid_params'])
-
-        if not self.config.train.freeze_generator_in_second_path:                
-            cycle_loss += 1.0 * F.mse_loss(recon_feats['shape_params'], flame_feats['shape_params']) 
-
-        losses['cycle_loss']  = cycle_loss
-        loss_second_path = losses['cycle_loss'] * self.config.train.loss_weights.cycle_loss
-
-        for key, value in losses.items():
-            losses[key] = value.item() if isinstance(value, torch.Tensor) else value
-
-
-        # ---------------- visualization struct ---------------- #
-        
-        outputs = {}
-        if batch_idx % self.config.train.visualize_every == 0:
-            outputs['2nd_path'] = torch.stack([rendered_img_2nd_path.detach().cpu().view(Ke, B,  C, H, W).permute(1, 0 , 2, 3, 4).reshape(-1, C, H, W), 
-                                             masked_img_2nd_path.detach().cpu().view(Ke, B, C, H, W).permute(1, 0 , 2, 3, 4).reshape(-1, C, H, W),
-                                             reconstructed_img_2nd_path.detach().cpu().view(Ke, B,  C, H, W).permute(1, 0 , 2, 3, 4).reshape(-1, C, H, W), 
-                                             rendered_img_2nd_path_2.detach().cpu().view(Ke, B, C, H, W).permute(1, 0 , 2, 3, 4).reshape(-1, C, H, W)], dim=1).reshape(-1, C, H, W)
-            
-        return outputs, losses, loss_second_path
 
     def freeze_encoder(self):
         utils.freeze_module(self.smirk_encoder.pose_encoder, 'pose encoder')
