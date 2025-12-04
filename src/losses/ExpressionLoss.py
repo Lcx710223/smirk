73720c2aa031877ae94c31ba1624c1dc37723dab
LCX@ARGOS <911239555@qq.com>
2025-12-04 20:27:34 +0800

    Add files via upload
    LCX251204，修改适配了PYTORCH2.6的安全要求。

---
+++ src/losses/ExpressionLoss.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+from src.losses.resnet import resnet50
+
+class ExpressionLoss(nn.Module):
+    def __init__(self, device="cpu"):
+        super(ExpressionLoss, self).__init__()
+        self.device = torch.device(device)
+
+        # 初始化 backbone
+        self.backbone = resnet50(num_classes=100, include_top=False, emoca_specific=True).eval()
+        self.backbone = self.backbone.to(self.device)
+
+        # 加载权重
+        # 加载权重
+        emotion_checkpoint = torch.load(
+            'assets/ResNet50/checkpoints/deca-epoch=01-val_loss_total/dataloader_idx_0=1.27607644.ckpt',
+            map_location=self.device,
+            weights_only=False
+        )['state_dict']
+
+        state_dict = {}
+        for k, v in emotion_checkpoint.items():
+            if k.startswith("backbone."):
+                new_k = k.replace("backbone.", "")
+                # 跳过 fc 层
+                if "fc.weight" in new_k or "fc.bias" in new_k:
+                  continue
+                state_dict[new_k] = v
+        self.backbone.load_state_dict(state_dict, strict=False)
+        self.loss_fn = nn.MSELoss()
+
+    def forward(self, pred, target):
+        feat_pred = self.backbone(pred.to(self.device))
+        feat_target = self.backbone(target.to(self.device))
+        return self.loss_fn(feat_pred, feat_target)
